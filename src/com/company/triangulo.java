765998e271c4696165b674c8fa3e66993e68b4c5
kikext <e-imbert@hotmail.com>
2018-01-08 20:13:38 +0100

    añadido triangulo

---
+++ src/com/company/triangulo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class triangulo {
+    posicion[] pos;
+
+    public triangulo(posicion[] pos) {
+        this.pos = pos;
+    }
+
+    public static void area(){}
+    public static void perimetro(){}
+
+}
