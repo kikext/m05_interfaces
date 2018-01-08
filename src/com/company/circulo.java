f8bee31904f050ed5c3907246816924b802fe855
kikext <e-imbert@hotmail.com>
2018-01-08 19:46:41 +0100

    añadidas coordenadas y constructores en ambos lados

--- src/com/company/circulo.java
+++ src/com/company/circulo.java
@@ -2,6 +2,14 @@ package com.company;
 
 public class circulo {
     private int radio;
+    private double coordX;
+    private double coordY;
+
+    public circulo(int radio, double coordX, double coordY) {
+        this.radio = radio;
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
 
     public static void area(){}
     public static void perimetro(){}
--- src/com/company/rectangulo.java
+++ src/com/company/rectangulo.java
@@ -3,6 +3,15 @@ package com.company;
 public class rectangulo{
     private int base;
     private int altura;
+    private double coordX;
+    private double coordY;
+
+    public rectangulo(int base, int altura, double coordX, double coordY) {
+        this.base = base;
+        this.altura = altura;
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
 
     public static void area(){}
     public static void perimetro(){}
